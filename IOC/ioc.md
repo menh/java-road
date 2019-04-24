b3a98da20540164ffd7791c1b2c96c81d30702e9
menhu <872204778@qq.com>
2019-04-24 18:23:04 +0800

    dd

---
+++ IOC/ioc.md
@@ -0,0 +1,4 @@
+###### IOC
+IOC (Inversion Of Control) 即控制反转
+控制指 控制javabean对象的声明周期
+反转指 将对象的控制权交给IOC
---
+++ IOC/spring-road/src/com/mh/Car.java
@@ -0,0 +1,8 @@
+package com.mh;
+
+public interface Car {
+	public void start();
+	public void turnRight();
+	public void turnLeft();
+	public void stop();
+}
